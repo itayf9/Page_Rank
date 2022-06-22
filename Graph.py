65cefd31fd2d7652d3ef6fc172c3ea34e48cf999
Itay Finkelshtein <itayf9@gmail.com>
2022-06-22 18:21:59 +0300

    Add comments

--- Graph.py
+++ Graph.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+
 import numpy as np
 import random
 
@@ -16,31 +16,38 @@ class Graph(object):
         self.vertices[node1-1]= np.append(self.vertices[node1-1], [node2])
 
     def isAdj(self, u, v):
+        """ Check if 'v' is a neighbor of 'u' """
+
         return True if v in self.vertices[u-1] else False
 
     def pageRank(self, numberOfSteps_N, numberOfIterations_t, prob_p):
+        """ Apply random walks on the graph """
+
         d=  np.zeros(self.vSize)
 
         for i in range(numberOfIterations_t):
             d[self.randomWalk(numberOfSteps_N, prob_p) - 1] +=1
 
         d/= numberOfIterations_t
-        d= np.around(d, decimals=3)
+        #for less digits in the values of the rank vector, use
+        #d= np.around(d, decimals=2)
 
         return d
 
     def randomWalk(self, numberOfSteps_N, prob_p):
+        """ Start walking through vertices 'N' moves """
+
         currentVertex = random.randint(1, self.vSize)
 
         for i in range(numberOfSteps_N):
-            if not self.isProbHit(prob_p): # 1-p
+            if not self.isProbHit(prob_p): # probability of (1-p)
                 countNumOfNeighbors = self.vertices[currentVertex-1].size
 
                 if countNumOfNeighbors != 0:
                     randSelectNeighbor = random.randint(0, countNumOfNeighbors-1)
                     currentVertex = self.vertices[currentVertex-1][randSelectNeighbor]
 
-            else: # p
+            else: # probability of p
                 currentVertex = random.randint(1, self.vSize)
 
 
@@ -49,6 +56,8 @@ class Graph(object):
 
 
     def isProbHit(self,prob_p):
+        """ Check if a new randomized number is within the probability that is given """
+
         currentRand = random.random()
 
         return True if currentRand < prob_p else False
--- main.py
+++ main.py
@@ -9,28 +9,26 @@ if __name__ == '__main__':
     G = Gr.Graph(2**12)
     q = 1/(2**12)
 
-    ''' add edges
+    ''' add edges - first graph family
+        for each edge (i,j), the probability of (i,j) to be added is 'q' 
+        we can select different values for 'q' '''
     for i in range(G.vSize):
         for j in range(G.vSize):
             if G.isProbHit(q):
                 G.add(i+1, j+1)
-    '''
 
-    qArr= [ 1/ (i+1) for i in range(G.vSize) ]
-    #qArrSQRT = [1 / math.sqrt(i + 1) for i in range(G.vSize)]
-    print(qArr)
-    for i in range(G.vSize):
-        for j in range(G.vSize):
-            if G.isProbHit(qArr[j]):
-                G.add(i + 1, j + 1)
+    ''' add edges - second graph family
+        for each edge(i,j), the probability of (i,j) to be added is qArr[j] 
+        we can select qArr of qArrSQRT '''
+    # qArr= [ 1/ (i+1) for i in range(G.vSize) ]
+    # qArrSQRT = [1 / math.sqrt(i + 1) for i in range(G.vSize)]
+    # for i in range(G.vSize):
+    #     for j in range(G.vSize):
+    #         if G.isProbHit(qArr[j]):
+    #             G.add(i + 1, j + 1)
 
-    '''
-    for k in range(114335747):
-        i = random.randint(1, G.vSize)
-        j = random.randint(1, G.vSize)
-        G.add(i, j)
-    '''
 
+    ''' random walks with 'e' stopping parameter'''
     ''' init parameters'''
     E = [0.5, 0.25, 0.125, 0.0625, 0.03125]
     p = E[0]
@@ -58,14 +56,29 @@ if __name__ == '__main__':
 
         print(np.linalg.norm(d - dprev))
 
+    # calculates the number of vertices with stable ranking
+    dPrevSeries = pd.Series(dprev)
+    dPrevSeries.sort_values(inplace=True, ascending=False)
+    dSeries = pd.Series(d)
+    dSeries.sort_values(inplace=True, ascending=False)
+    i=0
+    for x, y in zip(np.array(dPrevSeries.index),np.array(dSeries.index)):
+        if x==y:
+            i+=1
+        else:
+            break
+    print(dPrevSeries)
+    print(dSeries)
+    print(i)
 
 
 
+    ''' random walks with 'k' stopping parameter'''
     # ''' init parameters'''
     # K = [0.5, 0.25, 0.125, 0.0625, 0.03125]
     # p = K[0]
     # N = int(1 / p)
-    # k = int(1/K[0])
+    # k = int(1/K[4])
     # #dprev = np.zeros(G.vSize)
     # #dprevTopK = dprev[:k].copy()
     #
@@ -85,11 +98,12 @@ if __name__ == '__main__':
     #
     # dTopK = dSeries[:k].copy()
     #
-    # while not dTopK.equals(dprevTopK):
+    # while (not np.all(np.equal(np.array(dTopK.index), (np.array(dprevTopK.index))))) and (t < 8000000):
     #
     #     print("________________t=", t, "________________")
-    #     print((dSeries.where(dSeries.notna()))[:k])
-    #     print("________________end of t=", t, "________________")
+    #     # print((dSeries.where(dSeries.notna()))[:k])
+    #     # print("________________end of t=", t, "________________")
+    #     print(np.linalg.norm(d - dprev))
     #
     #     dprev = d.copy()
     #     dprevTopK = dTopK.copy()
@@ -98,7 +112,12 @@ if __name__ == '__main__':
     #     dSeries = pd.Series(d)
     #     dSeries.sort_values(inplace=True, ascending=False)
     #     dTopK = dSeries[:k].copy()
-
+    #
+    # print("________________t=", t, "________________")
+    # print((dSeries.where(dSeries.notna()))[:k])
+    # print("________________end of t=", t, "________________")
+    #
+    # print(np.linalg.norm(d - dprev))
 
 
 
